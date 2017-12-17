bc7e0e7bb1d87ed559c446c39dfec736edbd0fd2
darsen <danijel.arsenovski@gmail.com>
2017-12-17 13:15:03 -0800

    initial set of koans created

---
+++ PollyKoans/EnlightedEqualTo.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using System.Net;
+
+namespace PollyKoans
+{
+
+    public class Fill
+    {
+        public static object obj;
+        public static string _in;
+        public static int __in = 0;
+        public static HttpStatusCode status_code;
+    }
+
+    public class EnlightedEqualTo : EqualConstraint
+    {
+        private object expected;
+        private string enlightment = @"
+                        _ooOoo_
+                       o8888888o
+                       88"" . ""88
+                       (| -_- |)
+                       O\  =  /O
+                    ____/`---'\____
+                  .'  \\|     |//  `.
+                 /  \\|||  :  |||//  \
+                /  _||||| -:- |||||_  \
+                |   | \\\  -  /'| |   |
+                | \_|  `\`---'//  |_/ |
+                \  .-\__ `-. -'__/-.  /
+              ___`. .'  /--.--\  `. .'___
+           ."""" '<  `.___\_<|>_/___.' _> \"""".
+          | | :  `- \`. ;`. _/; .'/ /  .' ; |
+          \  \ `-.   \_\_`. _.'_/_/  -' _.' /
+===========`-.`___`-.__\ \___  /__.-'_.'_.-'================
+";
+        public EnlightedEqualTo(object expected) : base(expected)
+        {
+        }
+        public override ConstraintResult ApplyTo<TActual>(TActual actual)
+        {
+            var result = base.ApplyTo(actual);
+            if (result.IsSuccess)
+            {
+                return result;
+            }
+            return new ConstraintResult(this, "Śrāvaka");
+        }
+        public override string Description => enlightment;
+    }
+
+    public class Is_ : Is
+    {
+        public static EqualConstraint EqualTo_(object expected)
+        {
+            return new EnlightedEqualTo(expected);
+        }
+    }
+}
---
+++ PollyKoans/PollyKoan.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Polly;
+using System.Net.Http;
+using System.Net;
+using System.Threading;
+using Polly.Timeout;
+
+namespace PollyKoans
+{
+    public class PollyKoan
+    {
+        [Test]
+        public void Handle_and_Retry()
+        {
+            //Replace Fill.__in with value to make the test pass
+            var count = Fill.__in - 2;
+            Policy
+                .Handle<DivideByZeroException>()
+                .Retry(1)
+                .Execute(() => 8 / count++);
+                Assert.That(count, Is_.EqualTo_(2));
+        }
+
+        [Test]
+        public void Handle_and_Retry_With_Count()
+        {
+            var retries = 0;
+            var policy = Policy
+                .Handle<DivideByZeroException>()
+                .Retry(Fill.__in, (exception, retryCount, context) =>
+                {
+                    retries = retryCount;
+                });
+            try
+            {
+                policy.Execute(() => 8 / new int[]{0}.First());
+            }
+            catch
+            {
+                ++retries;
+            }
+            Assert.That(retries, Is_.EqualTo_(4));
+        }
+
+        [Test]
+        public void Handle_Result_with_Fallback() {
+            var policy = Policy
+            .HandleResult<HttpResponseMessage>(r => r.StatusCode == HttpStatusCode.NotFound)
+            .Fallback(new HttpResponseMessage(Fill.status_code));
+            var result = policy.Execute(() => {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            });
+            Assert.That(result.StatusCode, Is_.EqualTo_(HttpStatusCode.OK));
+        }
+
+        [Test]
+        public void Timeout() {
+            var result = "not timed out";
+            var policy = Policy
+            .Timeout(TimeSpan.FromMilliseconds(Fill.__in + 1000), TimeoutStrategy.Pessimistic);
+            try
+            {
+                policy.Execute(() => Thread.Sleep(500));
+            }
+            catch {
+                result = "timed out";
+            }
+            Assert.That(result, Is_.EqualTo_("timed out"));
+        }
+   
+    }
+}
