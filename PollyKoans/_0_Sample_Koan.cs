df3b2f03b4a394518184fa4f43fd5558df268fa9
darsen <danijel.arsenovski@gmail.com>
2018-09-16 11:36:03 -0700

    Upgraded .Net framework to 4.7.2. Upgraded NuGet package versions. Added Sample Koan.

---
+++ PollyKoans/_0_Sample_Koan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using Polly;
+using PollyKoans.Framework;
+
+namespace PollyKoans
+{
+    public class _0_Sample_Koan
+    {
+        [Test]
+        public void Sample_Koan_Unresolved()
+        {
+            //Replace FILL.__IN with 2
+            Assert.That(FILL.__IN, Is_.Equal_To(2));
+        }
+
+        [Test]
+        public void Sample_Koan_Resolved()
+        {
+            //FILL.__IN was replaced with 2
+            Assert.That(2, Is_.Equal_To(2));
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Polly-Koans
 Set of koans for learning App-vNext/Polly library.
+
+## Running
+Use the standard NUnit test runner to run these koans.
+
+## Troubleshooting
+
+### Project is targeting .Net framework 4.7 which is not installed
+Requires a .Net Framework 4.7. Installation using Chocolatey: 
+choco install dotnet4.7 -y
+choco install netfx-4.7.2-devpack -y
